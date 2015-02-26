f840d92a205d824ca97eb0fd9835e6cf8b29e876
Rohan <rlikhite@gmail.com>
2015-02-26 14:08:04 -0500

    word count chart added

--- js/dataLoad.js
+++ js/dataLoad.js
@@ -1,11 +1,43 @@
-new Chartist.Line('.ct-chart', {
-
-    labels: ['1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980','1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990','1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000','2001', '2002', '2003', '2004', '2005'],
+var data = {
+   labels: ['60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '00', '01', '02', '03', '04', '05', '06', '07'],
     series: [
-                    [0, 356, 90, 603, 1114, 11646, 630063, 546152, 23007, 18550, 21045, 27345, 43466, 150182, ]
+                    [7686,5452,7805,11506,6482,13028,23364,74839,148409,165896,250343,264378,230944,202774,166853,139120,99474,88537,76961,74200,67005,70666,58477,70251,58201,51870,55575,45818,49258,70236,66714,68303,64501,65268,69886,81921,103144,103121,101523,131237,125726,134192,153675,137837,148039,141211,133960,181166,110507]
   ]
-}, {
-    width: 960,
+};
+
+var options= {
     height: 300,
-    showArea: true
+    showArea: true,
+    showPoint: false,
+    axisY: {
+    // We can disable the grid for this axis
+    showGrid: true,
+    // and also don't show the label
+    showLabel: false
+  },
+        axisX: {
+    // We can disable the grid for this axis
+    showGrid: false,
+    // and also don't show the label
+    showLabel: true,
+        labelInterpolationFnc: function(value) {
+      return '"' + value ;
+    }
+  },
+    
+};
+        
+var chart = new Chartist.Line('.ct-chart',data, options);
+chart.on('draw', function(data) {
+  if(data.type === 'line' || data.type === 'area') {
+    data.element.animate({
+      d: {
+        begin: 2000 * data.index,
+        dur: 5000,
+        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+        to: data.path.clone().stringify(),
+        easing: Chartist.Svg.Easing.easeOutQuint
+      }
+    });
+  }
 });
